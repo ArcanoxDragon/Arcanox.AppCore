87dae7cd50ebaa4198ae172bf12f0e6b35ffa9e5
Arcanox <arcanox@arcanox.me>
2026-05-03 16:14:25 -0500

    Add extension to consume IOptionsMonitor<T> through an observable interface

---
+++ Arcanox.AppCore.Reactive/Extensions/OptionsMonitorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Reactive.Linq;
+using Arcanox.AppCore.Reactive.Internal;
+using Microsoft.Extensions.Options;
+
+namespace Arcanox.AppCore.Reactive.Extensions;
+
+public static class OptionsMonitorExtensions
+{
+	extension<T>(IOptionsMonitor<T> optionsMonitor)
+	{
+		/// <summary>
+		/// Returns an <see cref="IObservable{T}"/> that will produce a new value
+		/// any time the <see cref="IOptionsMonitor{TOptions}"/>'s options change.
+		/// The value produced will be a snapshot of the options after the change.
+		/// </summary>
+		public IObservable<T> WhenChanged()
+			=> Observable.Using(
+					() => new OptionsMonitorSubject<T>(optionsMonitor),
+					subject => subject.Observable)
+				.StartWith(optionsMonitor.CurrentValue);
+	}
+}
---
+++ Arcanox.AppCore.Reactive/Internal/OptionsMonitorSubject.cs
@@ -0,0 +1,28 @@
+﻿using System.Reactive.Subjects;
+using Microsoft.Extensions.Options;
+
+namespace Arcanox.AppCore.Reactive.Internal;
+
+internal class OptionsMonitorSubject<T> : IDisposable
+{
+	private readonly Subject<T>   subject = new();
+	private readonly IDisposable? subscription;
+
+	public OptionsMonitorSubject(IOptionsMonitor<T> optionsMonitor)
+	{
+		this.subscription = optionsMonitor.OnChange(OnChange);
+	}
+
+	public IObservable<T> Observable => this.subject;
+
+	public void Dispose()
+	{
+		this.subscription?.Dispose();
+		this.subject.Dispose();
+	}
+
+	private void OnChange(T value)
+	{
+		this.subject.OnNext(value);
+	}
+}
